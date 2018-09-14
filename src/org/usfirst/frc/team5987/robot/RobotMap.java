558ec963b005710d60402b9860127b478bb43353
yuval senderovitz <yuvalsan4545@gmail.com>
2018-09-14 12:03:34 +0300

    The code

---
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team5987.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+
+	/*done----------------DRIVE----------------*/
+	public static final int driveRightRearMotor = 8; // TODO: CHANGE!
+	public static final int driveRightFrontMotor = 9; // TODO: CHANGE!
+	public static final int driveLeftRearMotor = 6; // TODO: CHANGE!
+	public static final int driveLeftFrontMotor = 7; // TODO: CHANGE!
+	/*-------------------------------------*/
+
+	/*done---------------GRIPPER---------------*/
+	public static int gripperWheelLeft = 2; // TODO: CHANGE!
+	public static int gripperLeftPDP = 6; // TODO: CHANGE!
+	public static int gripperWheelRight = 3; // TODO: CHANGE!
+	public static int gripperRightPDP = 5; // TODO: CHANGE!
+	
+	public static final int[] gripperPDPs = new int[] {gripperLeftPDP, gripperRightPDP};
+	/*-------------------------------------*/
+
+	/*done----------------LIFT-----------------*/
+
+	public static int liftMotorPort = 2;
+
+	/*-------------------------------------*/
+
+	/*done----------------INTAKE---------------*/
+	public static final int intakeMotorLeft = 4;
+	public static int intakeLeftPDP = 4;
+	public static final int intakeMotorRight = 5;
+	public static int intakeRightPDP = 11;
+	
+	public static final int[] intakePDPs = new int[] {intakeLeftPDP, intakeRightPDP};
+	/*-------------------------------------*/
+
+	/*-----------------------------------SENSORS----------------------------------*/
+
+	/*done----------------DRIVE----------------*/
+
+	public static final int driveRightEncoderChannelA = 2; // TODO: CHANGE!
+	public static final int driveRightEncoderChannelB = 3; // TODO: CHANGE!
+	public static final int driveLeftEncoderChannelA = 0; // TODO: CHANGE!
+	public static final int driveLeftEncoderChannelB = 1; // TODO: CHANGE!
+	/*-------------------------------------*/
+
+	/*done---------------GRIPPER---------------*/
+	public static int proximityChannel = 0; // TODO: CHANGE!
+	/*-------------------------------------*/
+	
+	/*done----------------INTAKE---------------*/
+	public static final int intakeSolenoid1 = 0;
+	public static final int intakeSolenoid2 = 1;
+	// Solenoid for controlling the spring pistons.
+	public static final int intakeSolenoid3 = 2;
+	public static final int intakeSolenoid4 = 3;
+	/*-------------------------------------*/
+	
+}
---
+++ src/org/usfirst/frc/team5987/robot/subsystems/driveSubsystem.java
@@ -0,0 +1,43 @@
+
+package org.usfirst.frc.team5987.robot.subsystems;
+
+import org.usfirst.frc.team5987.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class driveSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	Victor MotorRightBack = new Victor(RobotMap.driveRightRearMotor);
+	Victor MotorRightFront = new Victor(RobotMap.driveRightFrontMotor);
+	Victor MotorLeftBack = new Victor(RobotMap.driveLeftRearMotor);
+	Victor MotorLeftFront = new Victor(RobotMap.driveLeftFrontMotor);
+	
+	public driveSubsystem(){
+		
+		MotorLeftBack.setInverted(true);
+    	MotorLeftFront.setInverted(true);
+    	MotorRightBack.setInverted(false);
+    	MotorRightFront.setInverted(false);
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void setSpeed(double RightSpeed,double LeftSpeed) {
+    	
+    	MotorLeftBack.set(LeftSpeed);
+    	MotorLeftFront.set(LeftSpeed);
+    	MotorRightBack.set(RightSpeed);
+    	MotorRightFront.set(RightSpeed);
+    	
+    }
+}
+
