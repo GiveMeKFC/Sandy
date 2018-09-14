f8cd83ed6165478b11f18c941ad08a3e3a44f356
yuval senderovitz <yuvalsan4545@gmail.com>
2018-09-14 12:04:08 +0300

    The code

--- src/org/usfirst/frc/team5987/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/DriveCommand.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team5987.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team5987.robot.Robot;
@@ -9,6 +10,8 @@ import org.usfirst.frc.team5987.robot.Robot;
  *
  */
 public class DriveCommand extends Command {
+	
+	Timer wait = new Timer();
 
     public DriveCommand() {
         // Use requires() here to declare subsystem dependencies
@@ -17,7 +20,9 @@ public class DriveCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	wait.reset();
+    	wait.start();
+    	Robot.driveSubsystem.setSpeed(0.05,0.05);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -25,12 +30,14 @@ public class DriveCommand extends Command {
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
+    protected boolean isFinished() {    	
+    	return wait.get() > 0.99;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	wait.stop();
+    	Robot.driveSubsystem.setSpeed(0,0);
     }
 
     // Called when another command which requires one or more of the same
