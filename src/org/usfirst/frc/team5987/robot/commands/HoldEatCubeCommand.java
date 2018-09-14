6a235e68d81ae1a944aebda55f1277d5408f213e
yuval senderovitz <yuvalsan4545@gmail.com>
2018-09-14 13:52:07 +0300

    Fix HoldEatCube command

--- src/org/usfirst/frc/team5987/robot/commands/HoldEatCubeCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/HoldEatCubeCommand.java
@@ -17,26 +17,30 @@ public class HoldEatCubeCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.intakeSubsystem.setSpeed(0.7);
-    	Robot.gripperSubsystem.setSpeed(0.7);
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-  
+    	Robot.intakeSubsystem.setSpeed(0.7);
+    	Robot.gripperSubsystem.setSpeed(0.7);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.intakeSubsystem.setSpeed(0);
+    	Robot.gripperSubsystem.setSpeed(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
+    	this.cancel();
     }
 }
